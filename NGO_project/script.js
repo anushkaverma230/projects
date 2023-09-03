110961df8fe7d3a0fb3bfcff1663a04361c1a26a
anushkaverma230 <123289562+anushkaverma230@users.noreply.github.com>
2023-09-03 12:57:50 +0530

    Add files via upload

---
+++ NGO_project/script.js
@@ -0,0 +1,41 @@
+// let valueDisplays = document.querySelectorAll(".lacNum");
+// let interval =2000;
+
+// valueDisplays.forEach((valueDisplay)=>{
+// let startValue = 0;
+// let endValue = parseInt(valueDisplay.getAttribute('data-val'));
+// let duration =Math.floor(interval / endValue);
+
+// let counter = setInterval(function() {
+//     startValue += 1;
+//     valueDisplay.textContent = startValue;
+//     if(startValue==endValue){
+//         clearInterval(counter);
+//     }
+// }, duration);
+// })
+// $('.Num').each(function() {
+//     var $this = $(this),
+//         countTo = $this.attr('data-count');
+    
+//     $({ countNum: $this.text()}).animate({
+//       countNum: countTo
+//     },
+  
+//     {
+  
+//       duration: 8000,
+//       easing:'linear',
+//       step: function() {
+//         $this.text(Math.floor(this.countNum));
+//       },
+//       complete: function() {
+//         $this.text(this.countNum);
+//         //alert('finished');
+//       }
+  
+//     });  
+    
+    
+  
+//   });
